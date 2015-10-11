2c659176e20d485cc0850515d1de96bcb0cea2d8
= <=>
2015-10-10 22:45:16 -0400

    Break standard methods out of SA file. Add Solver class to handle general operations for any solver

--- simulated_annealing.rb
+++ simulated_annealing.rb
@@ -1,76 +1,13 @@
-require_relative 'board'
+require_relative 'solver'
 
-class SimulatedAnnealingSolver
-  attr_reader :board
+class SimulatedAnnealing < Solver
 
-  # Basic Board Methods #
-
-  def self.from_file(filename)
-    board = Board.from_file(filename)
-    self.new(board)
+  def reachout
+    test_board = board.deep_dup
   end
 
-  def initialize(board)
-    @board = board
+  def solve
+
   end
-
-  def solved?
-    @board.solved?
-  end
-
-  # Randomize and score the board #
-
-  def randomize
-    @board.rows.flatten.each do |tile|
-      tile.value = rand(1..9) if tile.value.zero?
-    end
-  end
-
-  def randomize!
-    @board.rows.flatten.each do |tile|
-      tile.value = rand(1..9) unless tile.given?
-    end
-  end
-
-  def score
-    score = 0
-    board.rows.each do |row|
-      row_values = row.map { |el| el.value}
-      row_values.each do |el_value|
-        score -= 1 if row_values.count(el_value) == 1
-      end
-    end
-
-    board.columns.each do |col|
-      col_values = col.map { |el| el.value}
-      col_values.each do |el_value|
-        score -= 1 if col_values.count(el_value) == 1
-      end
-    end
-
-    score
-  end
-
-
-
-
-  ##For comparison purposes only##
-  def rand_solve
-    iteration = 0
-    until solved?
-      randomize!
-      iteration += 1
-      @board.render
-      puts "Score : #{score}"
-      puts "Iteration : #{iteration}"
-    end
-    puts "SOLVED!!!"
-    @board.render
-  end
-
-
-
-
-
 
 end
---
+++ solver.rb
@@ -0,0 +1,54 @@
+require_relative 'board'
+
+class Solver
+  attr_reader :board
+
+  # Basic Board Methods #
+
+  def self.from_file(filename)
+    board = Board.from_file(filename)
+    self.new(board)
+  end
+
+  def initialize(board)
+    @board = board
+  end
+
+  def solved?
+    @board.solved?
+  end
+
+  # Randomize and score the board #
+
+  def randomize
+    board.rows.flatten.each do |tile|
+      tile.value = rand(1..9) if tile.value.zero?
+    end
+  end
+
+  def randomize!
+    board.rows.flatten.each do |tile|
+      tile.value = rand(1..9) unless tile.given?
+    end
+  end
+
+  def score
+    score = 0
+    board.rows.each do |row|
+      row_values = row.map { |el| el.value}
+      row_values.each do |el_value|
+        score -= 1 if row_values.count(el_value) == 1
+      end
+    end
+
+    board.columns.each do |col|
+      col_values = col.map { |el| el.value}
+      col_values.each do |el_value|
+        score -= 1 if col_values.count(el_value) == 1
+      end
+    end
+
+    score
+  end
+
+end
