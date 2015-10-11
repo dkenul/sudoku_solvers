129f60730f3d5d32b0ffe5f49d82a5ada862d1df
= <=>
2015-10-11 12:56:20 -0400

    Add random swap method to SA solver. Add method to return valid index by square to board.

--- simulated_annealing.rb
+++ simulated_annealing.rb
@@ -2,8 +2,19 @@ require_relative 'solver'
 
 class SimulatedAnnealing < Solver
 
-  def reachout
+  def random_swap
+
     test_board = board.deep_dup
+    test_square = rand(0..8)
+
+    options = test_board.valid_square_indices(test_square).shuffle
+
+    s1 = options.first
+    s2 = options.last
+
+    test_board[s1], test_board[s2] = test_board[s2], test_board[s1]
+
+    test_board
   end
 
   def solve
